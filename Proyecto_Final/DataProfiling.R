df2e77c0acccd674753a7c145a451cb486c6018f
sss000x <Sinue.Flores@walmart.com>
2018-08-07 18:17:04 -0500

    project R

--- Proyecto_Final/DataProfiling.R
+++ Proyecto_Final/DataProfiling.R
@@ -1,4 +1,4 @@
-# función para obtener la moda, si la variable es numérica redondeamos a dos dígitos
+# Función para obtener la moda de una variable, si es caracter o factor y si la variable es numérica se redondea a los dos decimales más significativos
 my_mode <- function(x){
   if (class(x) %in% c("character", "factor")) {
     table(x) %>%
@@ -12,20 +12,20 @@ my_mode <- function(x){
   }
 }
 
-# función para hacer profiling del set de datos por columna, dependiendo de si son categóricas o numéricas
+# Función para hacer profiling del set de datos por columna, dependiendo de si son categóricas o numéricas
 profiling <- function(frame, type = "other"){
   # cardinalidad
   uniques <- data.frame(uniques = sapply(frame, function(x) unique(x) %>% length()))
   
-  # buscamos valores NA
+  # Se buscan valores NA
   nan <- data.frame(nan = sapply(frame, function(x) sum(is.na(x))))
   
   if (type == "categorical"){
     
-    # obtenemos la moda
+    # Se obtiene la moda
     mode <- data.frame(mode = sapply(frame, function(x) my_mode(x)))
     profiling_df <- cbind(uniques, nan, mode)
-    profiling_df #Recordar que, al ser esta la última instrucción, se devuelve este por lo que se comporta como return
+    profiling_df
   }
   else {
     profiling_df <- cbind(uniques, nan)
